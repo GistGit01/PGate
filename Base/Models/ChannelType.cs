2d0eb6be5ffc3d511c088dd847656ea93aa04add
Max-Wong <realmaxwong@gmail.com>
2025-07-02 14:33:41 +0800

    Init

---
+++ Base/Models/ChannelType.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Base.Models
+{
+    public enum ChannelType
+    {
+        SuPay = 1,
+        Omipay = 2
+    }
+}
---
+++ Base/Models/Merchant.cs
@@ -0,0 +1,7 @@
+﻿namespace Base.Models
+{
+    public class Merchant
+    {
+
+    }
+}
